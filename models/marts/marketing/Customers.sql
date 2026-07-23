8aedc2717d526e1a9a9ba79965a27ec94ccd5244
jatinder.singh@synechron.com.au <jatinder.singh@synechron.com.au>
2026-07-23 06:12:12 +0000

    added

--- models/marts/marketing/Customers.sql
+++ models/marts/marketing/Customers.sql
@@ -11,7 +11,7 @@ orders as (
     select
         *
 
-    from {{ ref('stg_jaffle_shop_orders') }}
+    from {{ ref('fact_orders') }}
 
 ),
 
@@ -22,7 +22,8 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
 
@@ -39,7 +40,9 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+       
 
     from customers
 
@@ -47,4 +50,4 @@ final as (
 
 )
 
-select * from final
+select *  from final 
